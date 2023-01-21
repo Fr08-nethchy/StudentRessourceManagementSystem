c6851decacca50ac96e657ea2b2ebb98bf5c68f0
ThessaNF <francoisthessanethchy@gmail.com>
2023-01-21 14:23:53 -0800

    ok

---
+++ src/test/java/studentressourcemanagementsystem/StudentRessourceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package studentressourcemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRessourceManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
